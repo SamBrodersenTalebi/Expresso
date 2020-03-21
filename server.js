64d32b2f3052594ac590b2fc4afa60ddeb9fe25a
samtalebi <samtalebi84@gmail.com>
2020-03-21 12:44:07 +0100

    create port variable and listen for port

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //import packages 
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 var cors = require('cors');
 const express = require('express');
 var morgan = require('morgan');
@@ -8,3 +7,14 @@ var errorhandler = require('errorhandler');
 
 //create instance of express app
 const app = express();
+
+//create PORT 
+const PORT = process.env.PORT || 4000;
+
+//Start server on port 
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at port ${PORT}`);
+});
+
+//Export app used in test file
+module.exports = app;
