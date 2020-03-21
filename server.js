320839d1abf3fd600615ea763005a2d9864c3330
samtalebi <samtalebi84@gmail.com>
2020-03-21 12:39:56 +0100

    install and import packages in server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+//import packages 
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+var cors = require('cors');
+const express = require('express');
+var morgan = require('morgan');
+var errorhandler = require('errorhandler');
