98c9fa81d8c7e97592cdb7d1ea810848e7bca3c2
samtalebi <samtalebi84@gmail.com>
2020-03-21 12:41:01 +0100

    instance of express app

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ var cors = require('cors');
 const express = require('express');
 var morgan = require('morgan');
 var errorhandler = require('errorhandler');
+
+//create instance of express app
+const app = express();
