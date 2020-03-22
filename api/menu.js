c5702bff1ea719e22349c0db6e0a5bfe93e1044e
samtalebi <samtalebi84@gmail.com>
2020-03-22 18:05:50 +0100

    put and delete handler /:menuID

--- api/menu.js
+++ api/menu.js
@@ -62,4 +62,35 @@ menuRouter.get('/:menuId', (req, res, next)=>{
     res.status(200).json({menu:req.menu});
 })
 
+menuRouter.put('/:menuId', (req, res, next)=>{
+    const title = req.body.menu.title;
+    if(!title){
+        return res.sendStatus(400);
+    }
+    const sql = 'UPDATE Menu SET title=$title WHERE Menu.id = $menuId';
+    const values = {$title:title, $menuId: req.params.menuId};
+
+    db.run(sql, values, function(error){
+        if(error){
+            next(error);
+        } else{
+            db.get(`SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`, (error, row)=>{
+                res.status(200).json({menu:row});
+            })
+        }
+    })
+});
+
+menuRouter.delete('/:menuId', (req, res, next)=>{
+    const sql = 'DELETE FROM Menu WHERE Menu.id = $menuId';
+    const values = {$menuId: req.params.menuId};
+    db.run(sql, values, function(error){
+        if (error){
+            next(error);
+        } else{
+            res.sendStatus(204);
+        }
+    })
+})
+
 module.exports = menuRouter;
