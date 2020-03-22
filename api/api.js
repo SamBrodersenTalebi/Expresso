472f189e6ab9b753ca76e112a648bda5cd8a157c
samtalebi <samtalebi84@gmail.com>
2020-03-22 16:10:33 +0100

    set up menuRouter

--- api/api.js
+++ api/api.js
@@ -3,8 +3,14 @@ const express = require('express')
 const apiRouter = express.Router()
 //import employeesRouter 
 const employeesRouter = require('./employees');
+//import menuRouter
+const menuRouter = require('./menu.js');
 
 //mount it at /employees
 apiRouter.use('/employees', employeesRouter)
+
+//mount menuRouter at /menus
+apiRouter.use('/menus', menuRouter);
+
 
 module.exports = apiRouter
---
+++ api/menu.js
@@ -0,0 +1,14 @@
+//import express and create instance of express router
+const express = require('express');
+const menuRouter = express.Router();
+
+//import sqlite3
+const sqlite3 = require('sqlite3');
+
+//check if process.env.TEST_DATABASE has been set, and if so load that database instead
+//it will be used for testing
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+
+
+module.exports = menuRouter;
