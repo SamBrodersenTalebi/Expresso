6a1ca2ecdebf8aa342717fa2fc5c86308b841176
samtalebi <samtalebi84@gmail.com>
2020-03-22 16:02:46 +0100

    put handler timesheet

--- api/timesheet.js
+++ api/timesheet.js
@@ -9,6 +9,25 @@ const timesheetRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+//router params
+timesheetRouter.param('timesheetId',(req, res, next, timesheetId)=>{
+    db.get('SELECT * FROM Timesheet WHERE Timesheet.id=$timesheetId',
+    {
+        $timesheetId:timesheetId
+    },(error, timesheet)=>{
+        if(error){
+            next(error)
+        }else if(timesheet){
+            //attach to request object as employee
+            req.timesheet = timesheet;
+            //move on to next function 
+            next();
+        } else{
+            res.sendStatus(404);
+        }
+    })
+})
+
 timesheetRouter.get('/', (req, res, next)=>{
     const sql = 'SELECT * FROM Timesheet WHERE Timesheet.employee_id = $employeeId';
     //have access to employeeId as we merged params
@@ -49,6 +68,39 @@ timesheetRouter.post('/', (req, res, next)=>{
             db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`, (error,row)=>{
                 //send it in the response body with 201 status code
                 res.status(201).json({timesheet:row});
+            })
+        }
+    })
+});
+
+timesheetRouter.put('/:timesheetId', (req, res, next)=>{
+    const hours = req.body.timesheet.hours;
+    const rate = req.body.timesheet.rate;
+    const date = req.body.timesheet.date;
+    const employeeId = req.params.employeeId;
+
+    if(!hours || !rate || !date){
+        return res.sendStatus(400);
+    }
+
+    const sql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date, employee_id = $employeeId WHERE Timesheet.id = $timesheetId';
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: employeeId,
+        $timesheetId: req.params.timesheetId
+    };
+    db.run(sql, values, function(error){
+        if(error){
+            next(error)
+        } else{
+            db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${req.params.timesheetId}`, (error,row)=>{
+                if(error){
+                    next(error);
+                } else{
+                    res.status(200).json({timesheet:row});
+                }
             })
         }
     })
