6438c2220ee94c44977abe0b6d24e1437892ece7
samtalebi <samtalebi84@gmail.com>
2020-03-23 11:18:16 +0100

    put handler menuItem

--- api/menuItem.js
+++ api/menuItem.js
@@ -9,6 +9,26 @@ const menuItemRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+
+//router params
+menuItemRouter.param('menuItemId',(req, res, next, menuItemId)=>{
+    db.get('SELECT * FROM MenuItem WHERE MenuItem.id=$menuItemId',
+    {
+        $menuItemId:menuItemId
+    },(error, menuItem)=>{
+        if(error){
+            next(error)
+        }else if(menuItem){
+            //attach to request object as employee
+            req.menuItemId = menuItemId;
+            //move on to next function 
+            next();
+        } else{
+            res.sendStatus(404);
+        }
+    })
+})
+
 menuItemRouter.get('/', (req,res,next)=>{
     const sql = 'SELECT * FROM MenuItem WHERE MenuItem.menu_id = $menuId';
     const values = {$menuId: req.params.menuId}
@@ -48,6 +68,39 @@ menuItemRouter.post('/', (req,res,next)=>{
         } else{
             db.get(`SELECT * FROM MenuItem WHERE MenuItem.id = ${this.lastID}`, (error,row)=>{
                 res.status(201).json({menuItem:row});
+            })
+        }
+    })
+});
+
+//put handler 
+menuItemRouter.put('/:menuItemId', (req, res, next)=>{
+    const name = req.body.menuItem.name;
+    const description = req.body.menuItem.description;
+    const inventory = req.body.menuItem.inventory;
+    const price = req.body.menuItem.price;
+    const menuId = req.params.menuId;
+
+    if(!name || !description || !inventory || !price){
+        return res.sendStatus(400);
+    }
+
+    const sql = "UPDATE MenuItem SET name=$name, description=$description, inventory=$inventory, price=$price, menu_id=$menuId WHERE MenuItem.id = $menuItemId";
+    const values = {
+        $name: name,
+        $description: description,
+        $inventory: inventory,
+        $price: price,
+        $menuId: menuId,
+        $menuItemId: req.params.menuItemId
+    };
+
+    db.run(sql, values, function(error){
+        if (error){
+            next(error);
+        } else{
+            db.get(`SELECT * FROM MenuItem WHERE MenuItem.id = ${this.lastID}`, (error, row)=>{
+                res.status(200).json({menuItem: row});
             })
         }
     })
